3903bffa8b7c4b1771f61230a1b6fabb0f8c0994
Eliud Garcia Ubuntu <jhonniereliudgarciacapera@gmail.com>
2025-11-04 17:22:02 -0500

    create news page

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://Eliud-Garcia.github.io/ProyectoFinal_2025IIg2_Cuellar_Santiago_y_Garcia_Jhonnier/"
 })
